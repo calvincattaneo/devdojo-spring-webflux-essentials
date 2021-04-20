2825c4326eba345aac6b87613b9ab60e6552cf17
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2021-04-20 06:10:45 -0300

    R2DBC Transactions Unit Tests

--- src/test/java/academy/devdojo/webflux/controller/AnimeControllerTest.java
+++ src/test/java/academy/devdojo/webflux/controller/AnimeControllerTest.java
@@ -16,6 +16,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
@@ -47,6 +49,10 @@ class AnimeControllerTest {
 
         BDDMockito.when(animeServiceMock.save(AnimeCreator.createAnimeToBeSaved()))
                 .thenReturn(Mono.just(anime));
+
+        BDDMockito.when(animeServiceMock
+                .saveAll(List.of(AnimeCreator.createAnimeToBeSaved(), AnimeCreator.createAnimeToBeSaved())))
+                .thenReturn(Flux.just(anime, anime));
 
         BDDMockito.when(animeServiceMock.delete(ArgumentMatchers.anyInt()))
                 .thenReturn(Mono.empty());
@@ -96,6 +102,17 @@ class AnimeControllerTest {
         StepVerifier.create(animeController.save(animeToBeSaved))
                 .expectSubscription()
                 .expectNext(anime)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("saveBatch creates a list of anime when successful")
+    public void saveBatch_CreatesListOfAnime_WhenSuccessful() {
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+
+        StepVerifier.create(animeController.saveBatch(List.of(animeToBeSaved, animeToBeSaved)))
+                .expectSubscription()
+                .expectNext(anime, anime)
                 .verifyComplete();
     }
 
