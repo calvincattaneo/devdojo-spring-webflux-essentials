beca3a406a33dea56f7af379116d5a9db0303d1c
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2021-04-20 07:04:43 -0300

    Reactive Security in memory

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -6,6 +6,7 @@ import academy.devdojo.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -23,6 +24,7 @@ public class AnimeController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ADMIN')")
     public Flux<Anime> listAll() {
         return animeService.findAll();
     }
